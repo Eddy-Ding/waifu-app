0743682c25cbc92c5a28d169d11fbbad59ac0a16
Eddy-Ding <eddyding4@gmail.com>
2024-04-03 20:25:38 -0400

    react set up

---
+++ src/App.js
@@ -0,0 +1,15 @@
+// App.js
+
+import React from 'react';
+import ChatComponent from './ChatComponent';
+
+const App = () => {
+  return (
+    <div>
+      <h1>React ChatGPT Integration</h1>
+      <ChatComponent />
+    </div>
+  );
+};
+
+export default App;
