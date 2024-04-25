d2c124a2c24c1ebe6c7de82fafd30f89b60bf6f2
davegii <ji60612@gmail.com>
2024-04-24 20:42:07 -0400

    added image generation

--- src/ChatComponent.js
+++ src/ChatComponent.js
@@ -68,7 +68,7 @@ const ChatComponent = () => {
   return (
     <div className="chat-container">
       <div className="picture-area">
-      <img src="https://via.placeholder.com/600" alt="Placeholder" style={{ width: '100%', height: '100%', objectFit: 'cover' }} />
+      <img src="https://oaidalleapiprodscus.blob.core.windows.net/private/org-AbNS1ZZMigCuXxTrDk5lbQuQ/user-4r6o9vFHa9aHuEpPOemBfRid/img-EK28SfZDJjMnTy6are0m1CAN.png?st=2024-04-24T23%3A20%3A19Z&se=2024-04-25T01%3A20%3A19Z&sp=r&sv=2021-08-06&sr=b&rscd=inline&rsct=image/png&skoid=6aaadede-4fb3-4698-a8f6-684d7786b067&sktid=a48cca56-e6da-484e-a814-9c849652bcb3&skt=2024-04-24T19%3A18%3A57Z&ske=2024-04-25T19%3A18%3A57Z&sks=b&skv=2021-08-06&sig=/mqJmK2USlRqJ1AQQLDmKg939tDb8VjwzqOsOHc2FQA%3D" alt="Placeholder" style={{ width: '100%', height: '100%', objectFit: 'cover' }} />
       </div>
       <div className="chat-area">
         <div className="settings-container">
